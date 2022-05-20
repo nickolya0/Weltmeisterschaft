29fbd8ca6e81ba892e8fc6ab06339ff36db458c5
Mykola Zitser <nix.ks.ua@gmail.com>
2022-05-20 19:32:41 +0200

    Tabelle_berechnen_1

--- tabelle.py
+++ tabelle.py
@@ -2,6 +2,7 @@ import os
 from textwrap import indent
 from tkinter import ttk
 from tkinter import *
+import tkinter
 
 import numpy as np
 
@@ -13,40 +14,70 @@ class Tabelle:
         self.load_fields_index = load_fields_index
         self.arr = []
         self.scoreList = []
+        self.spieleList = []
+        self.toreList = []
+        self.toreListNegativ = []
+        self.toreZusamenVerbinden = []
+
         if len(self.load_fields_index) == 0:
             self.load_fields()
         self.index_to_np()
 
         self.punkteBerechnenEigene()
+        self.toreZusamenFunk()
 
         print('---------------------------')
         print(load_fields_index)
         print(manschaftList_Sort)
         print('---------------------------')
+
+        self.initializeTabelle(self.root)
         
+        # tv = ttk.Treeview(self.root)
+        # tv['columns']=('Rank', 'Name', 'Badge')
+        # tv.column('#0', width=0, stretch=NO)
+        # tv.column('Rank', anchor=CENTER, width=80)
+        # tv.column('Name', anchor=CENTER, width=80)
+        # tv.column('Badge', anchor=CENTER, width=80)
+
+        # tv.heading('#0', text='', anchor=CENTER)
+        # tv.heading('Rank', text='Id', anchor=CENTER)
+        # tv.heading('Name', text='rank', anchor=CENTER)
+        # tv.heading('Badge', text='Badge', anchor=CENTER)
+
+        # tv.insert(parent='', index=1, iid=1, text='', values=('2','Anil','Bravo'))
+        # tv.insert(parent='', index=2, iid=2, text='', values=('3','Vinod','Charlie'))
+        # tv.insert(parent='', index=3, iid=3, text='', values=('4','Vimal','Delta'))
+        # tv.insert(parent='', index=4, iid=4, text='', values=('5','Manjeet','Echo'))
+        # tv.pack()
+
+    def initializeTabelle(self, root):
+        
+
         tv = ttk.Treeview(self.root)
-        tv['columns']=('Rank', 'Name', 'Badge')
+        tv['columns']=('Manschaft', 'Score', 'Tore')
         tv.column('#0', width=0, stretch=NO)
-        tv.column('Rank', anchor=CENTER, width=80)
-        tv.column('Name', anchor=CENTER, width=80)
-        tv.column('Badge', anchor=CENTER, width=80)
+        tv.column('Manschaft', anchor=CENTER, width=80)
+        tv.column('Score', anchor=CENTER, width=80)
+        tv.column('Tore', anchor=CENTER, width=80)
 
         tv.heading('#0', text='', anchor=CENTER)
-        tv.heading('Rank', text='Id', anchor=CENTER)
-        tv.heading('Name', text='rank', anchor=CENTER)
-        tv.heading('Badge', text='Badge', anchor=CENTER)
+        tv.heading('Manschaft', text='Manschaft', anchor=CENTER)
+        tv.heading('Score', text='Score', anchor=CENTER)
+        tv.heading('Tore', text='Tore', anchor=CENTER)
 
-        tv.insert(parent='', index=1, iid=1, text='', values=('2','Anil','Bravo'))
-        tv.insert(parent='', index=2, iid=2, text='', values=('3','Vinod','Charlie'))
-        tv.insert(parent='', index=3, iid=3, text='', values=('4','Vimal','Delta'))
-        tv.insert(parent='', index=4, iid=4, text='', values=('5','Manjeet','Echo'))
+        self.scoreList
+        self.spieleList
+        self.toreZusamenVerbinden
+
+        nummer = 0
+        for i in self.scoreList:
+            st = i.split(' ')[0]
+            print()
+            print()
+            tv.insert(parent='', index=nummer, iid=nummer, text='', values=(self.manschaftList_Sort[int(i.split(' ')[0])], self.scoreList[nummer].split(' ')[1], self.toreZusamenVerbinden[nummer].split(' ')[1]))
+            nummer += 1
         tv.pack()
-
-        # nummer = 0
-        # for i in list:
-        #     tv.insert(parent='', index=0, iid=0, text='', values=(nummer, ))
-        # nummer += 1
-        # tv.pack()
 
     def index_to_np(self):
         index = []
@@ -74,12 +105,13 @@ class Tabelle:
 
     def punkteBerechnenEigene(self):    # Eigene Spiel
         rows, columns = self.arr.shape
-        print("Rows = ",rows)
-        print("Columns = ", columns)
         x = 0
         str1 = ''
         while x < rows:
             score = 0
+            spiele = 0
+            tore = 0
+            toreN = 0
             for item in self.arr:
                 if int(item[1]) == x:
                     if int(item[2])>int(item[3]):
@@ -87,31 +119,55 @@ class Tabelle:
                     elif int(item[2])==int(item[3]):
                         score += 1
                     else:
-                        score += 0                
+                        score += 0  
+                    spiele += 1
+                    tore +=  int(item[2])           
+                    toreN += int(item[3])           
             for item in self.arr:
                 if int(item[4]) == x:
                     if int(item[3])>int(item[2]):
                         score += 3
-                        print('score-+3-', score)
+                        spiele += 1
                     elif int(item[3])==int(item[2]):
                         if int(item[4])!=int(item[1]):
                             score += 1
+                            spiele += 1
                     else:
                         score += 0
+                        spiele += 1
+                    print("tore--", tore)
+                    print("tore2--", toreN)
+                    tore += int(item[3])  
+                    toreN += int(item[2])  
+
             str1 = str(x) + ' ' + str(score)
-            self.scoreList.append(str1)
+            spl1 = str(x) + ' ' + str(spiele)
+            tore1 = str(x) + ' ' + str(tore)
+            tore2 = str(x) + ' ' + str(toreN)
+            self.scoreList.append(str1)     # score list
+            self.spieleList.append(spl1)    # spiele list
+            self.toreList.append(tore1)    # tore list
+            self.toreListNegativ.append(tore2)    # tore list
 
                 
             x += 1
-        print('TOTAL------', self.scoreList)
-            
+        print('SCORE------', self.scoreList)
+        print('SPIELE------', self.spieleList)
+        print('TORE------', self.toreList)
+        print('TORE-Negativ------', self.toreListNegativ)
 
-    def punkteBerechnenGast(self):      # Gast Spiel
-        for item in self.load_fields_index:
-            pass
-
-    def spieleBerechnen(self):
-        pass
+    def toreZusamenFunk(self):
+        str1 = ''
+        str2 = ''
+        q = 0
+        for i in self.toreList:
+                str1 = i.split(" ")
+                str2 = self.toreListNegativ[q].split(" ")
+                str3 = str1[0] + ' ' + str1[1] + ':' + str2[1]
+                self.toreZusamenVerbinden.append(str3)
+                q+=1
+        
+        print("self.toreZusamenVerbinden", self.toreZusamenVerbinden)
 
     def sBerechnen(self):
         pass
@@ -121,9 +177,7 @@ class Tabelle:
 
     def nBerechnen(self):
         pass
-
-    def toreBerechnen(self):
-        pass
+    
 
 
     # def tab_data_berechnen(self):
